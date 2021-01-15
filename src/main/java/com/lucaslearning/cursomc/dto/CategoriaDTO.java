3a8d4e99e89d05dd26943b091b6066111d25c1a8
luq11Z <lucas_segundinho@hotmail.com>
2021-01-15 20:52:55 +0000

    Listando todas categorias com DTO

---
+++ src/main/java/com/lucaslearning/cursomc/dto/CategoriaDTO.java
@@ -0,0 +1,39 @@
+package com.lucaslearning.cursomc.dto;
+
+import java.io.Serializable;
+
+import com.lucaslearning.cursomc.domain.Categoria;
+
+public class CategoriaDTO implements Serializable{
+	
+private static final long serialVersionUID = 1L;
+	
+	private Integer id; 
+	private String name;
+	
+	public CategoriaDTO() {
+		
+	}
+	
+	public CategoriaDTO(Categoria obj) {
+		id = obj.getId();
+		name = obj.getName();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
