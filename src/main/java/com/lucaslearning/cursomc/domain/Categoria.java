691e0bfd99367987c3b7da470b5e3edacb05639e
luq11Z <lucas_segundinho@hotmail.com>
2021-01-15 17:01:08 +0000

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/lucaslearning/cursomc/domain/Categoria.java
+++ src/main/java/com/lucaslearning/cursomc/domain/Categoria.java
@@ -10,8 +10,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 @Entity //dizendo ao jpa que essa clase vai ser uma tabela na bd
 public class Categoria implements Serializable{
 	
@@ -23,7 +21,6 @@ public class Categoria implements Serializable{
 	private String name;
 	
 	@ManyToMany(mappedBy = "categorias")
-	@JsonManagedReference
 	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {
