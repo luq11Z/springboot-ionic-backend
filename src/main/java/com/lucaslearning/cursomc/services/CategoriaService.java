cc94f83195aa27542421c81e8f105ab7fe618e28
luq11Z <lucas_segundinho@hotmail.com>
2021-01-16 14:09:33 +0000

    Correcoes: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/lucaslearning/cursomc/services/CategoriaService.java
+++ src/main/java/com/lucaslearning/cursomc/services/CategoriaService.java
@@ -16,48 +16,52 @@ import com.lucaslearning.cursomc.repositories.CategoriaRepository;
 import com.lucaslearning.cursomc.services.exceptions.DataIntegrityException;
 import com.lucaslearning.cursomc.services.exceptions.ObjectNotFoundException;
 
-
-@Service 
+@Service
 public class CategoriaService {
-	
-	@Autowired //injecao de dependencia, vai ser automaticamente instanciada pelo spring
+
+	@Autowired // injecao de dependencia, vai ser automaticamente instanciada pelo spring
 	private CategoriaRepository repository;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repository.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repository.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
 	}
-	
+
 	public void delete(Integer id) {
 		find(id);
 		try {
-		repository.deleteById(id);
+			repository.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível deletar uma categoria que possui produtos.");
 		}
 	}
-	
-	public List<Categoria> findAll(){
+
+	public List<Categoria> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repository.findAll(pageRequest);
 	}
-	
+
 	public Categoria fromDTo(CategoriaDTO objDto) {
-		return new Categoria(objDto.getId(), objDto.getNome()); //instancia uma categoria a partir de um DTO
+		return new Categoria(objDto.getId(), objDto.getNome()); // instancia uma categoria a partir de um DTO
+	}
+
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 }
