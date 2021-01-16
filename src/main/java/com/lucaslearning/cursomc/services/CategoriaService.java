324015086a193ac2ed8cb6d9a42f832e2433a6e1
luq11Z <lucas_segundinho@hotmail.com>
2021-01-16 12:43:09 +0000

    Validacao sintatica com Bean Validation

--- src/main/java/com/lucaslearning/cursomc/services/CategoriaService.java
+++ src/main/java/com/lucaslearning/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.lucaslearning.cursomc.domain.Categoria;
+import com.lucaslearning.cursomc.dto.CategoriaDTO;
 import com.lucaslearning.cursomc.repositories.CategoriaRepository;
 import com.lucaslearning.cursomc.services.exceptions.DataIntegrityException;
 import com.lucaslearning.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repository.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTo(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome()); //instancia uma categoria a partir de um DTO
 	}
 }
