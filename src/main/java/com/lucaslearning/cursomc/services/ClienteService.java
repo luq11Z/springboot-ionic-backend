33c47a4da0a008926ebf9ab7a6514bf5f13aef41
luq11Z <lucas_segundinho@hotmail.com>
2021-01-16 15:18:05 +0000

    Ajustando e testando a delecao de cliente

--- src/main/java/com/lucaslearning/cursomc/services/ClienteService.java
+++ src/main/java/com/lucaslearning/cursomc/services/ClienteService.java
@@ -56,7 +56,7 @@ public class ClienteService {
 		try {
 		repository.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível deletar porque há entidades relacionadas.");
+			throw new DataIntegrityException("Não é possível deletar porque há pedidos relacionados.");
 		}
 	}
 	
