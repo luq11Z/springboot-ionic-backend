d80e499a4aaed544444377598d5b57b15d28c2ed
luq11Z <lucas_segundinho@hotmail.com>
2021-01-19 15:50:54 +0000

    Criando profile de desenvolvimento

---
+++ src/main/java/com/lucaslearning/cursomc/config/DevConfig.java
@@ -0,0 +1,31 @@
+package com.lucaslearning.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.lucaslearning.cursomc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService service;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDataBase() throws ParseException {
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+		service.instantiateTestDataBase();
+		return true;
+	}
+}
