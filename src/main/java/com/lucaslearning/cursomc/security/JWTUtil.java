9e6f14fff282f55080ad3f51bda56bb5e0a040ad
luq11Z <lucas_segundinho@hotmail.com>
2021-02-19 09:51:58 +0000

    Implementando autorizacao

--- src/main/java/com/lucaslearning/cursomc/security/JWTUtil.java
+++ src/main/java/com/lucaslearning/cursomc/security/JWTUtil.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -21,11 +22,47 @@ public class JWTUtil {
 	 * Gerar token para um user
 	 */
 	public String generateToken(String username) {
+		expiration = expiration *6; 
 		return Jwts.builder()
 				.setSubject(username)
 				.setExpiration(new Date(System.currentTimeMillis() + expiration))
 				.signWith(SignatureAlgorithm.HS512, secret.getBytes())
 				.compact();
+	}
+	
+	/*
+	 * verificar a validade do token
+	 */
+	public boolean tokenValido(String token) {
+		Claims claims = getClaims(token);
+		if(claims != null) {
+			String username = claims.getSubject();
+			Date expirationDate = claims.getExpiration();
+			Date now = new Date(System.currentTimeMillis());
+			if(username != null && expirationDate != null && now.before(expirationDate)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public String getUsername(String token) {
+		Claims claims = getClaims(token);
+		if(claims != null) {
+			return claims.getSubject();
+		}
+		return null;
+	}
+	
+	/*
+	 * recuperar claims a partir de um token
+	 */
+	private Claims getClaims(String token) {
+		try {
+		return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody();
+		} catch (Exception e) {
+			return null;
+		}
 	}
 	
 }
