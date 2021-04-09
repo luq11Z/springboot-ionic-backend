8b3d88b29d60007a23e2bda625f723fda7f13eda
luq11Z <lucas_segundinho@hotmail.com>
2021-04-09 23:48:55 +0100

    Salvando URL da imagem em Cliente

--- src/main/java/com/lucaslearning/cursomc/domain/Cliente.java
+++ src/main/java/com/lucaslearning/cursomc/domain/Cliente.java
@@ -54,6 +54,8 @@ public class Cliente implements Serializable {
 	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>(); // colecoes nao se colocam nos construtores
+	
+	private String imgUrl;
 
 	public Cliente() {
 		addPerfis(Perfil.CLIENTE);
@@ -172,6 +174,14 @@ public class Cliente implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
 	}
 
 }
